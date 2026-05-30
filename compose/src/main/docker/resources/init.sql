523a580fc9931fe2222a8ec91dc351ebbaae8c60
David <115666355+David-237-9@users.noreply.github.com>
2026-05-30 12:28:05 +0100

    BD: University Data

--- compose/src/main/docker/resources/init.sql
+++ compose/src/main/docker/resources/init.sql
@@ -20,3 +20,18 @@ CREATE TABLE IF NOT EXISTS thesis (
     university_id UUID,
     FOREIGN KEY (university_id) REFERENCES university (id)
 );
+
+INSERT INTO university (name, repo_url) VALUES
+    ('Universidade Nova de Lisboa', 'https://run.unl.pt/server/oai/request'),
+    ('Universidade do Minho', 'https://repositorium.sdum.uminho.pt/oai/request'),
+    ('Universidade do Porto', 'https://repositorio-aberto.up.pt/oai/request'),
+    ('Universidade de Coimbra', 'https://estudogeral.uc.pt/oai/request'),
+    ('Universidade de Aveiro', 'https://ria.ua.pt/oai/request'),
+    ('Universidade da Beira Interior', 'https://ubibliorum.ubi.pt/server/oai/request'),
+    ('Universidade do Algarve', 'https://sapientia.ualg.pt/server/oai/request'),
+    ('Universidade de Évora', 'https://dspace.uevora.pt/server/rdpc/oai/request'),
+    ('Universidade de Trás-os-Montes e Alto Douro', 'https://repositorio.utad.pt/oai/request'),
+    ('Universidade dos Açores', 'https://repositorio.uac.pt/server/oai/request'),
+    ('Universidade da Madeira', 'https://digituma.uma.pt/server/oai/request'),
+    ('Universidade Aberta', 'https://repositorioaberto.uab.pt/server/oai/request'),
+    ('ISCTE - Instituto Universitário de Lisboa', 'https://repositorio.iscte-iul.pt/oai/request');
